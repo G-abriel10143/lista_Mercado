8bea197df3c8e826455fcb1cdc3e545e86b622e7
Gabriel Cerqueira Alves <101301397+Gabriel0030@users.noreply.github.com>
2022-05-18 12:13:19 -0300

    Update lista_mercado.js

--- lista_mercado.js
+++ lista_mercado.js
@@ -12,15 +12,33 @@ function adicionarItem() {
     li.setAttribute("id", qtdFilhos);
     li.setAttribute("onclick", "removerItem("+qtdFilhos+")");
     var valorInput = document.getElementById("inputTexto").value;
+    var dinheiroColocado = document.getElementById("inputDinheiro").value;
+   
     var text = document.createTextNode(valorInput);
+    var pegarReais = document.createTextNode(dinheiroColocado);
+    var concatenarTexto = document.createTextNode(valorInput + " - R$ " + dinheiroColocado);
     
-    li.appendChild(text);
-    ol.appendChild(li);
-    
+    if(dinheiroColocado <= 0) {
+        alert("Valor Invalido");
+        return;
+}
+    if(valorInput == ""){
+        alert("Insira o produto");
+        return;
+    }
+
+    li.append(concatenarTexto);
+    ol.append(li);
     document.getElementById("inputTexto").value = "";
+    document.getElementById("inputDinheiro").value = "";
+    calcularTotal(dinheiroColocado);
 }
 
+function calcularTotal(dinheiroColocado) {
+    var comprado = document.getElementById("totalComprado").textContent;
+    comprado = parseFloat(comprado) + parseFloat(dinheiroColocado);
+    document.getElementById("totalComprado").textContent = comprado;
+}
 function removerItem(idItem) {
     document.getElementById(idItem).remove();
-
 }
